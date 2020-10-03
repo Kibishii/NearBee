dc8e1f2da1f73af881848a80689e4f7c28ed897f
Timo Kramer <timokramer1@me.com>
2020-10-03 15:31:09 +0200

    Program code and documentation

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # NearBee
+
+Das Einhalten von Abständen zu anderen Menschen ist unter Pandemiebedingungen, wie sie derzeit durch COVID-19 hervorgerufen werden, besonders essenziell. 
+Jedoch wird nur sehr selten auf die
+notwendigen Abstände geachtet. Deswegen hat ein Team von
+Entwicklern einen Prototyp entwickelt, der dazu beitragen
+soll, eben diese Abstände im Alltag leichter einhalten zu
+können. In diesem Zuge wurde ein tragbares Modul, welches in Form einer Jacke getragen werden kann, entwickelt.
+
+Weitere allgemeine Informationen zum Produkt finden Sie auf unserer [Produktwebseite](https://kibishii.github.io/NearBee/).
+
+## Dokumentenverzeichnis
+
+- [Paper](docs/paper/01_Wissenschaftliches_Paper.pdf)
+- [Video](docs/video/01_Werbevideo.mp4)
+- [Evaluation](docs/evaluation)
+  - [Projektidee](docs/evaluation/01_Projektidee.pdf)
+  - [Personas und Userstories](docs/evaluation/02_Personas_und_Userstories.pdf)
+  - [Ergebnisse Vorabumfrage](docs/evaluation/03_Ergebnisse_Vorabumfrage.pdf)
+  - [Ergebnisse Abschlussumfrage](docs/evaluation/04_Ergebnisse_Abschlussumfrage.pdf)
+  - [Ergebnisse Werbevideo](docs/evaluation/05_Ergebnisse_Werbevideo.pdf)
+  - [Evaluation Umfragen](docs/evaluation/06_Evaluation_Umfragen.pdf)
---
+++ source/release/release.ino
@@ -0,0 +1,124 @@
+#include <Adafruit_NeoPixel.h>
+// Zuweisung der Board-Ausgaenge:
+
+#define trigPinB 0
+#define echoPinB 1
+
+#define trigPinR 2
+#define echoPinR 3
+
+#define trigPinL 6
+#define echoPinL 12
+
+#define vibratorL 9
+#define vibratorR 10
+
+#define led 8
+
+// Deklaration --- Start
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, led, NEO_GRB + NEO_KHZ800);
+int waitTime = 1;
+bool isVibL=false;
+bool isVibR=false;
+bool isVibB=false;
+int alarm =0;
+int savePosition =0;
+int cirtDistance = 150; // Default 150
+int warnDistance = 200; // Default 200
+int maxOkDistance = warnDistance +1;
+int distanceSaveR[] = {maxOkDistance, maxOkDistance, maxOkDistance};
+int distanceSaveL[] = {maxOkDistance, maxOkDistance, maxOkDistance};
+int distanceSaveB[] = {maxOkDistance, maxOkDistance, maxOkDistance};
+// Deklaration --- Ende
+
+void setup() {
+  // Zuweisung der In- und Outputs:
+  pinMode(trigPinR, OUTPUT);
+  pinMode(echoPinR, INPUT);
+  pinMode(trigPinB, OUTPUT);
+  pinMode(echoPinB, INPUT);
+  pinMode(trigPinL, OUTPUT);
+  pinMode(echoPinL, INPUT);
+  pinMode(vibratorR, OUTPUT);
+  pinMode(vibratorL, OUTPUT);
+  // Vorbereitung für das Onboard-Neopixel
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show();
+}
+
+void loop() {
+  // alarm = 1 bis 3 = Person auf mittlerer Entfernung; alaram = 4 und höher Person zu nahe
+  alarm =0;
+  // Berechung der Entfernungen
+  distanceSaveR[savePosition] = checkDistance(trigPinR, echoPinR);
+  distanceSaveL[savePosition] = checkDistance(trigPinL, echoPinL);
+  distanceSaveB[savePosition] = checkDistance(trigPinB, echoPinB);
+  
+  // Pruefen welche Vibratoren verwendet werden sollen
+  isVibR = checkSide(distanceSaveR, isVibR);
+  isVibL = checkSide(distanceSaveL, isVibL);
+  isVibB = checkSide(distanceSaveB, isVibB);
+
+  // Verwendung des Onboard-Neopixels
+  if(alarm >= 4){
+    strip.setPixelColor(0,strip.Color(255, 0, 0));
+  } else if(alarm >=1){
+    strip.setPixelColor(0,strip.Color(0, 0, 255));
+  }else{
+    strip.setPixelColor(0,strip.Color(0, 255, 0));
+  }
+  strip.show();
+
+  // Starten der Vibratoren
+  if(isVibB || isVibR){
+    digitalWrite(vibratorR, HIGH);
+  }else{
+    digitalWrite(vibratorR, LOW);
+  }
+  if(isVibB || isVibL){
+    digitalWrite(vibratorL, HIGH);
+  }else{
+    digitalWrite(vibratorL, LOW);
+  }
+  // Vorbereitung für den naechsten loop
+  savePosition +=1;
+  if(savePosition >= 3){
+    savePosition =0;
+  }
+  delay(waitTime);
+}
+
+// Ermittlung von Distanzen
+int checkDistance(int trigger, int echo){
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  long duration = pulseIn(echo, HIGH);
+  // Berechnung der Distanz:
+  int result =duration * 0.034 / 2;
+  return result;
+}
+
+// Pruefung ob eine Distanz unterschritten wurde
+bool checkSide(int distanceArray[], bool vib){
+  bool actVib = vib;
+  int distance = maxOkDistance;
+  for(int i; i <= sizeof(distanceArray); i++){
+    if(distance > distanceArray[i] && distanceArray[i] > 0){
+      distance = distanceArray[i];
+    }
+  }
+  if(distance <= cirtDistance){
+    actVib =true;
+    alarm +=4;
+  } else if(distance <=warnDistance){
+    actVib =!actVib;
+    alarm +=1;
+  }else{
+    actVib =false;
+  }
+  return actVib;
+}
